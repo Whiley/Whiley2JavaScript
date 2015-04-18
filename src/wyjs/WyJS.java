88fc2fec825462d22bd4f67b041c5a0a28ab00b5
Cody <Oathyy@gmail.com>
2015-04-18 17:49:21 +1200

    Revert "Revert "Updated so the javascript file is now a list of s""
    This reverts commit dc61b3954bb50cb9b05435e232013b7cafed90f1.

--- src/wyjs/WyJS.java
+++ src/wyjs/WyJS.java
@@ -2,6 +2,7 @@ package wyjs;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import wyil.io.WyilFilePrinter;
@@ -16,14 +17,19 @@ public class WyJS {
 
 	private WyilFile file;
 	private int indent = 0;
+	//Sorted list of strings that make up the javascript file
+	private ArrayList<String> js;
 
 	public WyJS(WyilFile file) {
 		this.file = file;
 		ArrayList<Block> blocks = new ArrayList<Block>(file.blocks());
+		js = new ArrayList<String>();
 		for (Block f : blocks) {
 			write(f);
 		}
 	}
+	
+
 
 	private void write(Block e){
 		
@@ -41,43 +47,44 @@ public class WyJS {
 				i++;
 			}
 			str.append("){//" + func.type().toString() +"\n");
+			js.add(str.toString());
 			indent++;
-				//System.out.println(c.body().);
-				Iterator iter = func.body().iterator();
-				while(iter.hasNext()){
-					Object tmp = iter.next();
-					write(tmp, str);
-				}
+			Iterator iter = func.body().iterator();
+			while(iter.hasNext()){
+				Object tmp = iter.next();
+				write(tmp);
+			}
 			indent--;
-			str.append(getIndentBlock() + "}");
+			js.add(getIndentBlock() + "}");
 			
-
-			System.out.println(str);
+			for(String s: js){
+				System.out.print(s);
+			}
 		}
 	}
 	
-	private void write(Object o, StringBuilder str){
+	private void write(Object o){
 		if(o instanceof Codes.Const){
-			write((Codes.Const) o, str);
+			write((Codes.Const) o);
 		}else if(o instanceof Codes.BinaryOperator){
-			write((Codes.BinaryOperator) o, str);
+			write((Codes.BinaryOperator) o);
 		}else if(o instanceof Codes.Assign){
-			write((Codes.Assign) o, str);
+			write((Codes.Assign) o);
 		}else if(o instanceof Codes.Return){
-			write((Codes.Return) o, str);
+			write((Codes.Return) o);
 		}else{
 			System.out.println("Unknown object " + o.getClass());
 		}
 	}
 	
-	private void write(Codes.Const o, StringBuilder str){
-		str.append(getIndentBlock() + "var r" + o.target() + " = " + o.constant + ";//" + o.toString() +"\n");
+	private void write(Codes.Const o){
+		js.add(getIndentBlock() + "var r" + o.target() + " = " + o.constant + ";//" + o.toString() +"\n");
 	}
 	
-	private void write(Codes.BinaryOperator o, StringBuilder str){
-		str.append(getIndentBlock() + "var r" + o.target() + "= ");
+	private void write(Codes.BinaryOperator o){
+		js.add(getIndentBlock() + "var r" + o.target() + " = ");
 		if(o.kind == Codes.BinaryOperatorKind.ADD){
-			str.append("r" + o.operand(0) + " + " + "r" + o.operand(1) + ";//" + o.toString() +"\n");
+			js.add("r" + o.operand(0) + " + " + "r" + o.operand(1) + ";//" + o.toString() +"\n");
 		}else if(o.kind == Codes.BinaryOperatorKind.BITWISEAND){
 			System.out.println("Cant do BitWiseAnd");
 		}else if(o.kind == Codes.BinaryOperatorKind.BITWISEOR){
@@ -85,11 +92,11 @@ public class WyJS {
 		}else if(o.kind == Codes.BinaryOperatorKind.BITWISEXOR){
 			System.out.println("Cant do BitWiseXOR");
 		}else if(o.kind == Codes.BinaryOperatorKind.DIV){
-			str.append("r" + o.operand(0) + " / " + "r" + o.operand(1) + ";//" + o.toString() +"\n");
+			js.add("r" + o.operand(0) + " / " + "r" + o.operand(1) + ";//" + o.toString() +"\n");
 		}else if(o.kind == Codes.BinaryOperatorKind.LEFTSHIFT){
 			System.out.println("Cant do LeftShift");
 		}else if(o.kind == Codes.BinaryOperatorKind.MUL){
-			str.append("r" + o.operand(0) + " * " + "r" + o.operand(1) + ";//" + o.toString() +"\n");
+			js.add("r" + o.operand(0) + " * " + "r" + o.operand(1) + ";//" + o.toString() +"\n");
 		}else if(o.kind == Codes.BinaryOperatorKind.RANGE){
 			System.out.println("Cant do Range");
 		}else if(o.kind == Codes.BinaryOperatorKind.REM){
@@ -97,21 +104,20 @@ public class WyJS {
 		}else if(o.kind == Codes.BinaryOperatorKind.RIGHTSHIFT){
 			System.out.println("Cant do RightShift");
 		}else if(o.kind == Codes.BinaryOperatorKind.SUB){
-			str.append("r" + o.operand(0) + " - " + "r" + o.operand(1) + ";//" + o.toString() +"\n");
+			js.add("r" + o.operand(0) + " - " + "r" + o.operand(1) + ";//" + o.toString() +"\n");
 		}else {
 			System.out.println("Unknown kind");
 		}
 	}
 	
-	private void write(Codes.Return o, StringBuilder str){
-		//str.append(getIndentBlock() + "var r" + getNameNum() + " = " + o.constant + ";\n");
+	private void write(Codes.Return o){
 		if(o.operand!=-1){
-			str.append(getIndentBlock() + "return r" + o.operand + ";//" + o.toString() +"\n");
+			js.add(getIndentBlock() + "return r" + o.operand + ";//" + o.toString() +"\n");
 		}
 	}
 	
-	private void write(Codes.Assign o, StringBuilder str){
-		//TODO: not yet 
+	private void write(Codes.Assign o){
+		js.add(getIndentBlock() + "var r" + o.target() + " = r" + o.operand(0) + ";//" + o.toString() +"\n");
 	}
 	
 	private String getIndentBlock(){
