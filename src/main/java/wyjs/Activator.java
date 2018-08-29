542ed92ed6b42850f3621a25f8d36802d7b73ba6
DavePearce <david.pearce@ecs.vuw.ac.nz>
2018-08-29 15:23:29 +1200

    Implement Build.Platform
    This adds support for the new Build.Platform implemented in WyCC.  This
    takes control away from this module for managing the project and build
    rules.  It also allows for the wy.toml file, etc.

--- src/main/java/wyjs/Activator.java
+++ src/main/java/wyjs/Activator.java
@@ -16,12 +16,16 @@ package wyjs;
 import java.io.IOException;
 
 import wybs.lang.Build;
+import wybs.lang.Build.Project;
 import wybs.lang.Build.Task;
+import wybs.util.AbstractCompilationUnit.Value;
 import wyc.lang.WhileyFile;
+import wycc.cfg.Configuration;
 import wycc.lang.Module;
 import wycc.util.Logger;
 import wyfs.lang.Content;
 import wyfs.lang.Path;
+import wyfs.lang.Path.ID;
 import wyfs.lang.Content.Type;
 import wyfs.util.Trie;
 import wyjs.core.JavaScriptFile;
@@ -29,11 +33,21 @@ import wyjs.tasks.JavaScriptCompileTask;
 
 public class Activator implements Module.Activator {
 
+	private static Trie DEBUG_CONFIG_OPTION = Trie.fromString("build/js/debug");
+	private static Trie TARGET_CONFIG_OPTION = Trie.fromString("build/js/target");
+	private static Trie SOURCE_CONFIG_OPTION = Trie.fromString("build/whiley/target");
+
 	// =======================================================================
 	// Build Platform
 	// =======================================================================
 
 	private static Build.Platform JS_PLATFORM = new Build.Platform() {
+		// Specify directory where generated JS files are dumped.
+		private Trie source = Trie.fromString("bin");
+		// Specify directory where generated JS files are dumped.
+		private Trie target = Trie.fromString("bin/js");
+		// Specify whether debug mode enabled or not.
+		private boolean debug = true;
 
 		@Override
 		public String getName() {
@@ -41,8 +55,33 @@ public class Activator implements Module.Activator {
 		}
 
 		@Override
+		public Configuration.Schema getConfigurationSchema() {
+			return Configuration.fromArray(
+					Configuration.UNBOUND_BOOLEAN(DEBUG_CONFIG_OPTION, "Set debug mode (default is ON)", false),
+					Configuration.UNBOUND_STRING(TARGET_CONFIG_OPTION, "Specify location for generated JavaScript files", false));
+		}
+
+		@Override
+		public void apply(Configuration configuration) {
+			//
+			if(!configuration.matchAll(DEBUG_CONFIG_OPTION).isEmpty()) {
+				debug = configuration.get(Value.Bool.class, Trie.fromString(DEBUG_CONFIG_OPTION)).get();
+			}
+			if(!configuration.matchAll(TARGET_CONFIG_OPTION).isEmpty()) {
+				String path = configuration.get(Value.UTF8.class, Trie.fromString(TARGET_CONFIG_OPTION)).unwrap();
+				target = Trie.fromString(path);
+			}
+			if(!configuration.matchAll(SOURCE_CONFIG_OPTION).isEmpty()) {
+				String path = configuration.get(Value.UTF8.class, Trie.fromString(SOURCE_CONFIG_OPTION)).unwrap();
+				source = Trie.fromString(path);
+			}
+		}
+
+		@Override
 		public Task initialise(Build.Project project) {
-			return new JavaScriptCompileTask(project);
+			JavaScriptCompileTask task = new JavaScriptCompileTask(project);
+			task.setDebug(debug);
+			return task;
 		}
 
 		@Override
@@ -67,12 +106,17 @@ public class Activator implements Module.Activator {
 
 		@Override
 		public Path.Root getSourceRoot(Path.Root root) throws IOException {
-			return root.createRelativeRoot(Trie.fromString("bin"));
+			return root.createRelativeRoot(source);
 		}
 
 		@Override
 		public Path.Root getTargetRoot(Path.Root root) throws IOException {
-			return root.createRelativeRoot(Trie.fromString("bin/js"));
+			return root.createRelativeRoot(target);
+		}
+
+		@Override
+		public void execute(Project project, ID path, String name, Value... args) {
+			throw new IllegalArgumentException("native JavaScript execution currently unsupported");
 		}
 	};
 
